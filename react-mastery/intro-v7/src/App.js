327eff04dda4cf2a1648bac8523e02f12fd237fc
Zain-ul-din <zain.personal47@gmail.com>
2022-08-08 19:25:31 +0500

    new tools

--- react-mastery/intro-v7/src/App.js
+++ react-mastery/intro-v7/src/App.js
@@ -1,32 +1,26 @@
 // Entry Point
 "use strict" 
 
-const e = React.createElement;
+const e = React.createElement 
 
-const TODOS = [
-    'do some work' ,
-    'sleep' ,
-    'coding' ,
-    'go fast'
-]
+const TODOS = ["do some work", "sleep", "coding", "go fast"] 
 
-const Todo = (props) => e ('h1' , {} , props.name)
+const Todo = (props) => e("h1", {}, props.name) 
 
 const Todos = () => {
-    return e(
-        'div' , 
-        {className : 'todos'} ,
-        TODOS.map ((todo , idx) => e( Todo , {name : todo , key : idx}) ) // passing props
-    )
-}
+  return e(
+    "div",
+    { className: "todos" },
+    TODOS.map((todo, idx) => e(Todo, { name: todo, key: idx })) // passing props
+  ) 
+} 
 
-const container = document.querySelector ("#root");
-const root = ReactDOM.createRoot (container)
+const container = document.querySelector("#root") 
+const root = ReactDOM.createRoot(container) 
 
-root.render (e('div' , {} , [
-    e ('button' , {key : 0} , 'click me')
-    ,e(Todos , {key : 1}) 
-]))
+root.render(
+  e("div", {}, [e("button", { key: 0 }, "click me"), e(Todos, { key: 1 })])
+) 
 
 /// DOCS
 
@@ -39,6 +33,11 @@ root.render (e('div' , {} , [
 
   "use strict" // compile code in strict mode 
   Example :-
-    x = 3.14;       // This will not cause an error.
+    x = 3.14        // This will not cause an error.
   
 */
+
+// rc stand for run command
+// npm i -D eslint@8.8.0 eslint-config-prettier@8.3.0
+// parcel
+  
