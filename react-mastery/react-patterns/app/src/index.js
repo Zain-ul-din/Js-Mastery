e1f01aaad31b4d98c9d259995d894d3585619f43
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-09-10 22:07:00 +0500

    react-patterns

---
+++ react-mastery/react-patterns/app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<>React Patterns</>);
