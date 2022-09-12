30aa35f55a6e1b0388c8525b180de8890da53eb8
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-09-12 22:01:09 +0500

    render-props

--- react-mastery/react-patterns/app/src/components/App.tsx
+++ react-mastery/react-patterns/app/src/components/App.tsx
@@ -1,8 +1,13 @@
 import React  from 'react'
+import RenderProps from '../Patterns/RenderProps';
 
 export default function App () : JSX.Element {
     return (
-        <> React Patterns </>
+        <> 
+        React Patterns 
+        {/*Render Props Pattern*/}
+        <RenderProps /> 
+        </>
     )
 }
 
